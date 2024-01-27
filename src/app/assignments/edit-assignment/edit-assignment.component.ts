8b073ff0a0143da145da0f82436e0b379487349b
alessandrov24 <92879771+alessandrov24@users.noreply.github.com>
2024-01-27 20:15:02 +0100

    Nouvelles propriétés ajoutés dans edit-assignment.component.ts

--- src/app/assignments/edit-assignment/edit-assignment.component.ts
+++ src/app/assignments/edit-assignment/edit-assignment.component.ts
@@ -13,6 +13,14 @@ export class EditAssignmentComponent implements OnInit {
  assignment!: Assignment | undefined;
  nomAssignment!: string;
  dateDeRendu!: Date;
+ // Nouvelles propriétés
+ auteur!: string;
+ matiere!: string;
+ imageMatiere!: string;
+ photoProf!: string;
+ note?: number;
+ remarques!: string;
+ // 
  canEdit: boolean = false;
 
  constructor(
@@ -43,6 +51,13 @@ export class EditAssignmentComponent implements OnInit {
       // Pour pré-remplir le formulaire
       this.nomAssignment = assignment.nom;
       this.dateDeRendu = assignment.dateDeRendu;
+      // Pré remplir avec les nouvelles propriétés
+      this.auteur = assignment.auteur;
+      this.matiere = assignment.matiere;
+      this.imageMatiere = assignment.imageMatiere;
+      this.photoProf = assignment.photoProf;
+      this.note = assignment.note;
+      this.remarques = assignment.remarques;
     });
   }
   onSaveAssignment() {
@@ -51,6 +66,15 @@ export class EditAssignmentComponent implements OnInit {
     // on récupère les valeurs dans le formulaire
     this.assignment.nom = this.nomAssignment;
     this.assignment.dateDeRendu = this.dateDeRendu;
+    // récupération des nouvelles propriétés
+    this.assignment.auteur = this.auteur;
+    this.assignment.matiere = this.matiere;
+    this.assignment.imageMatiere = this.imageMatiere;
+    this.assignment.photoProf = this.photoProf;
+    this.assignment.note = this.note;
+    this.assignment.remarques = this.remarques;
+
+   
     this.assignmentsService
       .updateAssignment(this.assignment)
       .subscribe((message) => {
