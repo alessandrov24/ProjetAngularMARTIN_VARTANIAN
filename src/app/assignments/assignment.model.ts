268be4c51326ef9323fc178eb1be89c7b7a6e11b
alessandrov24 <92879771+alessandrov24@users.noreply.github.com>
2024-01-27 19:59:01 +0100

    Nouvelles propriétés dans assignment.model.ts

--- src/app/assignments/assignment.model.ts
+++ src/app/assignments/assignment.model.ts
@@ -6,4 +6,14 @@ export class Assignment {
   nom!:string;
   dateDeRendu!:Date;
   rendu!:boolean;
+
+   // Nouvelles propriétés
+  auteur!: string; // Nom de l'élève
+  matiere!: string; // Matière de l'assignment (ex. Base de données, Technologies Web, etc.)
+  imageMatiere?: string; // URL ou chemin d'accès à l'image associée à la matière
+  photoProf?: string; // URL ou chemin d'accès à la photo du prof
+  note?: number; // Note sur 20. Optionnelle jusqu'à ce que l'assignment soit rendu et noté
+  remarques?: string; // Remarques sur l'assignment
+
+  
 }
