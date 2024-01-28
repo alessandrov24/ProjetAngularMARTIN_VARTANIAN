69bb7da126126885d10a301dab8644f5723e23ee
alessandrov24 <92879771+alessandrov24@users.noreply.github.com>
2024-01-28 21:50:40 +0100

    Comment add-assignment.component.ts

--- src/app/assignments/add-assignment/add-assignment.component.ts
+++ src/app/assignments/add-assignment/add-assignment.component.ts
@@ -1,18 +1,21 @@
+// Importations nécessaires depuis les modules Angular et autres services
 import { Component, EventEmitter,  Output} from '@angular/core';
 import { Assignment } from '../assignment.model';
 import { AssignmentsService } from 'src/app/shared/assignments.service';
 import { Router } from '@angular/router';
-
+// Décorateur Component définissant le sélecteur, le template HTML et le style CSS pour ce composant
 @Component({
   selector: 'app-add-assignment',
   templateUrl: './add-assignment.component.html',
   styleUrls: ['./add-assignment.component.css']
 })
 export class AddAssignmentComponent {
+  // Déclaration et initialisation des propriétés utilisées pour créer un nouvel assignment
   nomDevoir:string = "";
   dateRendu:Date = new Date();
   nouvelAssignment:Assignment;
   //@Output() nouvelAssignment = new EventEmitter<Assignment>();
+  // Ces propriétés suivantes sont ajoutées pour prendre en charge les nouvelles propriétés des assignments
   auteur: string = "";
   matiere: string = "";
   imageUrlMatiere: string = "";
@@ -24,6 +27,7 @@ export class AddAssignmentComponent {
 
   constructor(private assignmenntService:AssignmentsService, private router:Router) { }
 
+  // Méthode appelée lors de la soumission du formulaire
   onSubmit() {
     const newAssignment = new Assignment();
     newAssignment.id = Math.floor(Math.random() * 1000000);
